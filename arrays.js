c2c6ad0998daf3248669ef045e5c8c9ba66e1e88
faithmwamkinga <faithmwamkinga@gmail.com>
2023-03-17 15:44:06 +0300

    arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+/*Arrays
+Find the last element of the following arrays.
+arr1 = [3,7,34,90,12]
+arr2 = [true, "green", "where",12,56]
+ */
+let arr1 = [3,7,34,90,12]
+console.log(arr1.lastIndexOf(12));
+arr2 = [true, "green", "where",12,56]
+
